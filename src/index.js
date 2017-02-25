a95e7d60d8f0ebb9137ce1abc9bf7e8bafed5dfa
Matthew Carlson <matthewfcarlson@gmail.com>
2017-02-25 14:52:48 -0800

    changed a few things

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 'use strict';
 
-const app = require('./app');
 const port = process.env.PORT || app.get('port');
-const server = app.listen(port);
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const socketio = require('feathers-socketio');
+const handler = require('feathers-errors/handler');
+const bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+const service = require('feathers-mongodb');
 
-server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
-);
+// Create a feathers instance.
+const app = feathers()
+  // Enable Socket.io
+  .configure(socketio())
+  // Enable REST services
+  .configure(rest())
+  // Turn on JSON parser for REST services
+  .use(bodyParser.json())
+  // Turn on URL-encoded parser for REST services
+  .use(bodyParser.urlencoded({extended: true}));
+
+
+const promise = new Promise(function(resolve) {
+  // Connect to your MongoDB instance(s)
+  MongoClient.connect('mongodb://localhost:27017/melay').then(function(db){
+    // Connect to the db, create and register a Feathers service.
+    app.use('/messages', service({
+      Model: db.collection('messages'),
+      paginate: {
+        default: 2,
+        max: 4
+      }
+    }));
+
+    // A basic error handler, just like Express
+    app.use(handler());
+
+    // Start the server
+    var server = app.listen(port);
+    server.on('listening', function() {
+      resolve(server);
+    });
+  }).catch(function(error){
+    console.error(error);
+  });
+});
+
+module.exports = promise;
