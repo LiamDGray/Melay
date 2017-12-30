82ba7c98fbdf60d75b6882978bb92403645d61be
Matthew Carlson <matthewfcarlson@gmail.com>
2017-12-29 18:22:12 -0800

    added in the reducers from snowflake

--- client/App.js
+++ client/App.js
@@ -1,33 +1,49 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList,ListView, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry
+} from "react-native";
+//router flux
+import {Router, Scene} from 'react-native-router-flux'
+//REDUX
+import {Provider} from 'react-redux'
+//the store
+import configureStore from './lib/configureStore'
+//actions
+import {setPlatform, setVersion} from './reducers/device/deviceActions'
+import {setStore} from './reducers/global/globalActions'
+
+var VERISON = pack.VERSION
+
+const styles = StyleSheet.create({
+  tabBar: {
+    height: 70
+  }
+})
+
+function getInitialState() {
+  const _initState = {
+    auth: new AuthInitialState(),
+    device: new DeviceInitialState().set("isMobile", true),
+    global: new GlobalInitialState(),
+    profile: new ProfileInitialState()
+  };
+  return _initState;
+}
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true
-    }
-  }
-
-  componentDidMount() {
-    return fetch('https://facebook.github.io/react-native/movies.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson.movies),
-        }, function() {
-          // do something with new state
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+   
 
   render() {
-    if (this.state.isLoading) {
+    const store = configureStore(getInitialState());
+
+    store.dispatch(setPlatform(platform));
+    store.dispatch(setVersion(VERSION));
+    store.dispatch(setStore(store));
+
+    /*if (this.state.isLoading) {
       return (
         <View style={{flex: 1, paddingTop: 20}}>
           <ActivityIndicator />
@@ -42,6 +58,20 @@ export default class App extends React.Component {
           renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
         />
       </View>
+    );*/
+
+    return (
+      <Provider store={store}>
+        <Router>
+          <Sceen key='root' hideNavBar>
+          <Scene key='App'
+                component={App}
+                type='replace'
+                initial />
+          </Router>
+      </Provider>
     );
   }
 }
+
+AppRegistry.registerComponent("Melay", () => App);
